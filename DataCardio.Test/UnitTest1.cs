8a9dbfd0fb101d47d8fe097a7c67e6b73def9734
studenti <studenti@rosselli.loc>
2019-04-08 10:16:40 +0200

    Add project files.

---
+++ DataCardio.Test/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCardio.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MAX1()
+        {
+            string errore = " ";
+            int eta = 20;
+            double return_max = EquazioniLibrary.DataCardio.FrequenzaBattitoCardiacoMAX(eta, ref errore);
+            double aspetativa_max = 140;
+            string aspetativa_errore = "Non esiste l'eta minire di 0 ";
+
+            if (errore == " ")
+            {
+                Assert.AreEqual(aspetativa_max, return_max);
+            }
+            else
+            {
+                Assert.AreEqual(aspetativa_errore, errore);
+            }
+
+        }
+        [TestMethod]
+
+        public void MAX2()
+        {
+            string errore = " ";
+            int eta = -5;
+            double return_max = EquazioniLibrary.DataCardio.FrequenzaBattitoCardiacoMAX(eta, ref errore);
+            double aspetativa_max = 0;
+            Assert.AreEqual(aspetativa_max, return_max);
+            string aspetativa_errore = "Non esiste l'eta minire di 0 ";
+
+            if (errore == " ")
+            {
+                Assert.AreEqual(aspetativa_max, return_max);
+            }
+            else
+            {
+                Assert.AreEqual(aspetativa_errore, errore);
+            }
+
+        }
+        [TestMethod]
+        public void MAX3()
+        {
+            string errore = " ";
+            int eta = -1;
+            double return_max = EquazioniLibrary.DataCardio.FrequenzaBattitoCardiacoMAX(eta, ref errore);
+            double aspetativa_max = 0;
+            Assert.AreEqual(aspetativa_max, return_max);
+            string aspetativa_errore = "Non esiste l'eta minire di 0 ";
+
+            if (errore == " ")
+            {
+                Assert.AreEqual(aspetativa_max, return_max);
+            }
+            else
+            {
+                Assert.AreEqual(aspetativa_errore, errore);
+            }
+
+        }
+
+
+
+
+        [TestMethod]
+        public void Min1()
+        {
+            int eta = 20;
+            double return_max = EquazioniLibrary.DataCardio.FrequenzaBattitoCardiacoMIN(eta);
+            double aspetativa_max = 180;
+            Assert.AreEqual(aspetativa_max, return_max);
+        }
+
+
+    }
+}
---
+++ EquazioniLibrary/DataCardio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public class DataCardio
+    {
+        public static double FrequenzaBattitoCardiacoMAX(int eta, ref string errore)
+        {
+
+            double x = 220 - eta;
+            double max = x * 0.7;
+
+            if (x > 220)
+            {
+                max = 0;
+                errore = "Non esiste l'eta minire di 0 ";
+            }
+            return max;
+        }
+
+
+
+        public static double FrequenzaBattitoCardiacoMIN(int eta)
+        {
+            double x = 220 - eta;
+            double min = x * 0.9;
+            return min;
+        }
+
+
+    }
+}
